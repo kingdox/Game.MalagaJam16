82390ba1ff9e22386b238f9959b330546ce21583
Xavier Arpa <arpaxavier@gmail.com>
2023-07-29 12:27:33 +0200

    -

---
+++ Assets/Source/Code/Scripts/Modules/Bootstrap/Bootstrap.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Kingdox.UniFlux;
+//using Service;
+using XavHelpTo;
+
+public sealed class Bootstrap : MonoFlux
+{
+    //protected override void OnFlux(bool condition) {}
+    //[Flux()] private void Method() => ;
+    private static Bootstrap _;
+    private IEnumerator Start()
+    {
+
+        yield return 
+    }
+}
