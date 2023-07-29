6bcfa32023a1d34801275c49602f33ad6dddbdb2
Sergio CM <sergiocmdev@gmail.com>
2023-07-29 22:36:42 +0200

    cosis

--- Assets/Source/Code/Scripts/Modules/Dia_n/DiaN_Flux.cs
+++ Assets/Source/Code/Scripts/Modules/Dia_n/DiaN_Flux.cs
@@ -1,17 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using Kingdox.UniFlux;
-//using Service;
 
 public sealed class DiaN_Flux : MonoFlux
 {
-    //protected override void OnFlux(bool condition) {}
-    private void Method() 
+    [SerializeField] private TextWriter textWriter;
+    [SerializeField] private Canvas canvas;
+
+    [SerializeField] private TextScriptableObject textScriptableObject;
+
+    private void Awake()
+    {
+        // canvas.enabled = false;
+    }
+
+    private void Start()
+    {
+        Method();
+    }
+
+    private void Method()
     {
         "DayN".GetState(out int daysLeft);
-        
+        var originalText = textScriptableObject.Text;
+        var formattedText = string.Format(originalText, daysLeft);
+        textWriter.SetText(formattedText);
+        textWriter.StartWrite();
     }
 }
