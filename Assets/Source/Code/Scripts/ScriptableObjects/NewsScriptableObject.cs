06b1402025f3021b4fb98fd9b1f2c662b52ff5a9
Xavier Arpa <arpaxavier@gmail.com>
2023-07-29 13:45:10 +0200

    -

--- Assets/Source/Code/Scripts/ScriptableObjects/NewsScriptableObject.cs
+++ Assets/Source/Code/Scripts/ScriptableObjects/NewsScriptableObject.cs
@@ -8,7 +8,19 @@ using XavHelpTo;
 public class NewsScriptableObject : ScriptableObject
 {
     [SerializeField] [TextArea] private string _debug_info;
+
+    [Header("Cosas que se usan en ChooseScene")]
+    [Space]
     public TextScriptableObject Text_Summary;
+    public TextScriptableObject Text_Title;
+    public TextScriptableObject Text_Description;
+    
+    [Header("Cosas que se usan en Map (la parte de las noticias)")]
+    [Space]
     public TextScriptableObject[] Text_Quotes;
+    
+    // [Header("Cosas que se usan en Map (la parte de la charla con la pareja)")]
+    // [Space]
+    // public TextScriptableObject[] Text_Quotes;
 
 }
