61ec35bb3288964b86286b9a6034ee7f3ba86dc8
Juan Trave <juantravemed@gmail.com>
2023-07-30 13:51:45 +0200

    no message

--- Assets/Source/Code/Scripts/Components/NewsRandomSelector.cs
+++ Assets/Source/Code/Scripts/Components/NewsRandomSelector.cs
@@ -16,16 +16,38 @@ public class NewsRandomSelector : MonoBehaviour
         // list_news_left = general.News.ToArray().ToList();
     }
 
-    // int dia = 1;
 
-    // public NewsRandomSelector() 
-    // {
-    //     Random rnd = new Random(6);
-    //     int aux = rnd;
-    //     if(dia == 1)
-    //     {
-    //         lst.RemoveAt(rnd);   //eliminar por posicion
-    //         rnd.Next(1,6);
-    //     }
-    // }
+
+    // int dia = 1;
+    public NewsRandomSelector() 
+    {
+        int randomIndex1 = Random.Range(0, general.list_news_left.Length);
+        int randomIndex2 = Random.Range(0, general.list_news_left.Length);
+
+        // Evitar seleccionar el mismo índice dos veces
+        while (randomIndex2 == randomIndex1)
+        {
+            randomIndex2 = Random.Range(0, newsData.newsArray.Length);
+        }
+
+        // Obtener las noticias seleccionadas
+        string selectedNews1 = newsData.newsArray[randomIndex1];
+        string selectedNews2 = newsData.newsArray[randomIndex2];
+
+        // Mostrar las noticias seleccionadas en la consola
+        Debug.Log("Noticias seleccionadas: ");
+        Debug.Log(selectedNews1);
+        Debug.Log(selectedNews2);
+
+            // Eliminar las noticias seleccionadas del arreglo
+            newsData.newsArray[randomIndex1] = null;
+            newsData.newsArray[randomIndex2] = null;
+
+            // Si todas las noticias se han borrado, mostrar mensaje y salir
+            if (AllNewsDeleted())
+            {
+                Debug.Log("Todas las noticias han sido borradas.");
+                return;
+            }
+    }
 }
